cbd5db1bdebe9af1d83f6c9762dd65e3d3740e0c
Dtown <dannyotown@gmail.com>
2020-10-03 18:45:47 -0500

    Pushing all files for deploy

---
+++ src/views/housepage/HouseInfo.jsx
@@ -0,0 +1,14 @@
+import { MDBCard, MDBCardHeader } from "mdbreact";
+import React from "react";
+
+function HouseInfo() {
+  return (
+    <>
+      <MDBCard className="w-75 mx-auto">
+        <MDBCardHeader>6053 Tholozan Ave</MDBCardHeader>
+      </MDBCard>
+    </>
+  );
+}
+
+export default HouseInfo;
--- src/views/housepage/HousePageView.js
+++ src/views/housepage/HousePageView.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HouseBox from "./HouseBox";
+import HouseInfo from "./HouseInfo";
 
 function HousePage() {
   return (
     <>
       <HouseBox />
+      <HouseInfo />
     </>
   );
 }
